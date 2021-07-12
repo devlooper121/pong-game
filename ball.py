76bacbae37e4a9e4850ecc1ba548cad85a90530f
Devlooper121 <mailtodheeraj2@gmail.com>
2021-07-12 10:44:39 +0530

    function for play/pause

--- ball.py
+++ ball.py
@@ -6,13 +6,15 @@ class Ball(Turtle):
         super().__init__()
         self.penup()
         self.shape("circle")
+        self.pause = 0
         self.x_move = 10
         self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + self.x_move
-        new_y = self.ycor() + self.y_move
-        self.goto(new_x, new_y)
+        if self.pause == 0:
+            new_x = self.xcor() + self.x_move
+            new_y = self.ycor() + self.y_move
+            self.goto(new_x, new_y)
 
     def y_bounce(self):
         self.y_move *= -1
@@ -25,3 +27,8 @@ class Ball(Turtle):
         self.x_move *= -1
         # self.y_move *= -1
 
+    def play_pause(self):
+        if self.pause == 0:
+            self.pause = 1
+        else:
+            self.pause = 0
